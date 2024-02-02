4c7d20969b8b6fd6a2706103fab320b07e860c10
noproblem2311 <quanpva.dev@gmail.com>
2024-02-02 13:33:42 +0700

    setup update

--- src/views.py
+++ src/views.py
@@ -2,7 +2,11 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import cloudinary.uploader
 
+import os
 from datetime import datetime
+
+
+
 
 @csrf_exempt
 def handle_video(request):
@@ -10,26 +14,19 @@ def handle_video(request):
         video_chunk = request.FILES.get('chunks')
         if video_chunk:
             current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-            file_name = f'webcam_{current_time}'
-            print("type video", type(video_chunk))
-            # Đọc nội dung của chunk video
-            video_content = video_chunk.read()
+            file_name = f'webcam_{current_time}.mp4'  
 
-            # Tải lên Cloudinary sử dụng nội dung của video
-            try:
-                upload_result = cloudinary.uploader.upload(
-                    video_content,
-                    resource_type='video',
-                    public_id=file_name,
-                    format='mp4'  # Đảm bảo bạn chỉ định đúng định dạng nếu cần
-                )
+            upload_result = cloudinary.uploader.upload_large(
+                video_chunk,
+                resource_type='video',
+                public_id=file_name
+            )
 
-                if upload_result.get('secure_url'):
-                    return JsonResponse({'status': 'success', 'url': upload_result['secure_url']})
-            except Exception as e:
-                print(e)  # In lỗi ra console để debug
+            if upload_result.get('secure_url'):
+                print(upload_result['secure_url'])
+                return JsonResponse({'status': 'success', 'url': upload_result['secure_url']})
+            else:
                 return JsonResponse({'status': 'error', 'message': 'Upload failed'})
-
         else:
             return JsonResponse({'status': 'error', 'message': 'No file received'})
     else:
